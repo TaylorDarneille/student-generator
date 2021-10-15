bd47c9dfe11edf5b81da52065be8dff8444fbd56
TaylorDarneille <tmdarneille@gmail.com>
2021-10-15 08:42:48 -0700

    updated student roster

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const students = ["Timothy", "Emily", "Ramon", "Eduardo", "Azikoue", "Michael", "Kelly", "Paulina", "Hannah", "Shuzel", "Steven", "Christos", "Isaac", "Juan", "Lawrence", "Rafal", "Devonte", "Cory", "Justin", "Hector", "Galyver", "Dagem", "Devin", "Derrick", "Lindsay", "Jason", "Goro", "Dino", "Jonathan", "Mackenzie", "Ana", "Isaac", "Jerry"]
+const students = ["Timothy", "Emily", "Ramon", "Eduardo", "Azikoue", "Michael", "Kelly", "Paulina", "Hannah", "Shuzel", "Steven", "Christos", "Isaac", "Juan", "Lawrence", "Devonte", "Cory", "Justin", "Hector", "Galyver", "Dagem", "Devin", "Derrick", "Lindsay", "Jason", "Goro", "Dino", "Jonathan", "Mackenzie", "Ana", "Isaac", "Jerry"]
 
 const newRandomStudent = () => {
     return students[Math.floor(Math.random()*students.length)]
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    console.log(`Number of students: ${students.length}`)
     button.addEventListener('click', ()=>{
         console.log("hullo")
         result.innerText = newRandomStudent()
